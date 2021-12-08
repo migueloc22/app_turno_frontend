a1886f44d74ca133600e3d260c24636eed77a315
migueloc22 <migueloc22@gmail.com>
2021-12-08 11:20:16 -0500

    estrutura de proyecto

---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template >
+  <v-container color="#000">
+    <form @submit.prevent="submit">
+        <v-text-field :append-icon="mdi-close" placeholder="Email"></v-text-field>
+    </form>
+  </v-container>
+</template>
+
+<script>
+// import App from '../App.vue'
+export default {
+  //   components: { App },
+  data() {
+      return {
+          form:{
+              email:'',
+              pass:'',
+          }
+      }
+  },
+};
+</script>
+
+<style>
+</style>
